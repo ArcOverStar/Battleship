1251a9295b9e7b3250a64fb2abfca66ec6cabcaf
ArcOverStar <55924792+ArcOverStar@users.noreply.github.com>
2020-01-19 17:25:38 +0530

    Add files via upload

--- BATTLESHIP.py
+++ BATTLESHIP.py
@@ -1,9 +1,20 @@
 import random, sys
 UB=[]
 CB=[]
-
+TB =  [["0","0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0","0"]]
 ship = {'C':["Carrier",5],'B':["Battleship",4],'S':["Submarine",3],'c':["Cruiser",3],'D':["Destroyer",2]}
 symbol = list(ship.keys())
+hit = False
+end = False
 
 def user_set_board():
     B = [["0","0","0","0","0","0","0","0","0","0"],
@@ -22,8 +33,10 @@ def user_set_board():
     confirm = False
     ori =''
     conf = ''
+    
     while (not confirm):
-        for i in range(0,5):   
+        for i in range(0,5): 
+                chk=0
                 print("Input ",ship[symbol[i]][0],"\'s coordinates",sep='')
                 while(not placed):
                     x = int(input("Enter x coordinate(0-9): "))
@@ -33,15 +46,27 @@ def user_set_board():
                         n = ship[symbol[i]][1]
                         
                         if (ori == "h" and ((x+n-1)<10)):
-                            for j in range (0,n):
-                                B[y][x+j]= symbol[k]
-                            k+=1
-                            placed = True     
+                            for j in range(0,n):
+                                if(B[y][x+j]=="0"):
+                                    chk=chk+1
+                            if (chk==n):
+                                for j in range (0,n):
+                                    B[y][x+j]= symbol[k]
+                                k+=1
+                                placed = True  
+                            else:
+                                print("Ship already in those coordnates")
                         elif (ori == "v" and ((y+n-1)<10)):
-                            for j in range (0,n):
-                                B[y+j][x]= symbol[k]
-                            k+=1
-                            placed = True
+                            for j in range(0,n):
+                                if(B[y+j][x]=="0"):
+                                    chk=chk+1
+                            if (chk==n):
+                                for j in range (0,n):
+                                    B[y+j][x]= symbol[k]
+                                k+=1
+                                placed = True
+                            else:
+                                print("Ship already in those coordnates")
                         else:
                             print("There is not enough tiles for the input coordinates and orientation!")
                     else:
@@ -73,7 +98,8 @@ def comp_set_board():
     k = 0 #Helps in the initialization of user's pieces
     placed = False    
     ori =''
-    for i in range(0,5):   
+    for i in range(0,5): 
+            chk=0
             while(not placed):
                 x = random.randint(0,9)
                 y = random.randint(0,9)
@@ -82,29 +108,40 @@ def comp_set_board():
                     n = ship[symbol[i]][1]
                     
                     if (ori == 0 and ((x+n-1)<10)):
-                        for j in range (0,n):
-                            B[y][x+j]= symbol[k]
-                        k+=1
-                        placed = True     
+                        for j in range(0,n):
+                                if(B[y][x+j]=="0"):
+                                    chk=chk+1
+                        if (chk==n):
+                            for j in range (0,n):
+                                B[y][x+j]= symbol[k]
+                            k+=1
+                            placed = True     
                     elif (ori == 1 and ((y+n-1)<10)):
-                        for j in range (0,n):
-                            B[y+j][x]= symbol[k]
-                        k+=1
-                        placed = True
+                        for j in range(0,n):
+                                if(B[y+j][x]=="0"):
+                                    chk=chk+1
+                        if (chk==n):
+                            for j in range (0,n):
+                                B[y+j][x]= symbol[k]
+                            k+=1
+                            placed = True
                     
             placed = False
     return B
 
 def user_attack(x,y):
         hcount = 0
-        
-        if (CB[y][x]==0):
-            print ("It missesd!We'll get em next time.")
+        global end
+        global CB
+        global hit
+        if (CB[y][x]=="0"):
+            print ("It missesd at (",x,",",y,") We'll get em next time.",sep='')
             TB[y][x]=='x'
             hit = True
             return TB
         elif(CB[y][x] in symbol):
             print("We hit!")
+            hcount+=1
             TB[y][x]="o"
             hit = True
             if (hcount==17):
@@ -116,46 +153,47 @@ def user_attack(x,y):
                 print("   **      **    **  *********   **  **  **   ***     ***      ** ***")
                 print("   **      ********  *********   ****  **** ********  ***       *****")
                 end = True
+            else:
+                end = False
         else:
             print("You've already attacked there. Attack somewhere else.")
         Ret = [end,TB,hit]
         return Ret
 def comp_attack():
+    global end
+    global UB
     hit = False
-    compxcount=0
+    comphcount=0
     while (not hit):
         x,y=random.randint(0,9),random.randint(0,9)
-        if (UB[y][x]==0):
+        if (UB[y][x]=="0"):
             UB[y][x]=='x'
-            print("The enemy missed!")
+            print("The enemy missed at (",x,",",y,")",sep='')
             hit = True
         elif(UB[y][x] in symbol):
-            print("Gah the enemy hit us!")
+            print("Gah the enemy hit us at (",x,",",y,")",sep='')
             UB[y][x]="o"
-            if(compxcount==17):
-                print("***  ***   ********  ***   ***   ***         ********  ********   ********")
-                print(" **  **    **    **  ***   ***   ***         **    **  ********   ********")
-                print("  ***      **    **  ***   ***   ***         **    **  **         **")
-                print("  ***      **    **  ***   ***   ***         **    **  ********   ********")
-                print("  ***      **    **  ***   ***   ***         **    **        **   **")
-                print("  ***      **    **  ***   ***   ***         **    **  ********   ********")
-                print("  ***      ********  *********   ********    ********  ********   ********")
+            comphcount+=1
+            hit = True
+            if(comphcount==17):
+                print("***  ***   ********  ***   ***   ***       ********  ********  ********")
+                print(" **  **    **    **  ***   ***   ***       **    **  ********  ********")
+                print("   **      **    **  ***   ***   ***       **    **  **        **")
+                print("   **      **    **  ***   ***   ***       **    **  ********  ********")
+                print("   **      **    **  ***   ***   ***       **    **        **  **")
+                print("   **      **    **  *********   ***       **    **  ********  ********")
+                print("   **      ********  *********   ********  ********  ********  ********")
                 end = True
-        ret = [end,UB]
+            else:
+                end = False
+        ret = [end,UB,hit]
         return ret
 def main():
-    end = False
-    hit = False
-    TB = [["0","0","0","0","0","0","0","0","0","0"],
-      ["0","0","0","0","0","0","0","0","0","0"],
-      ["0","0","0","0","0","0","0","0","0","0"],
-      ["0","0","0","0","0","0","0","0","0","0"],
-      ["0","0","0","0","0","0","0","0","0","0"],
-      ["0","0","0","0","0","0","0","0","0","0"],
-      ["0","0","0","0","0","0","0","0","0","0"],
-      ["0","0","0","0","0","0","0","0","0","0"],
-      ["0","0","0","0","0","0","0","0","0","0"],
-      ["0","0","0","0","0","0","0","0","0","0"]]
+    global end
+    global hit
+    global TB
+    global UB  
+    global CB
     print("*************************BATTLESHIP*********************************")
     print("1:Start Game")
     print("2:Quit Game")
@@ -164,6 +202,7 @@ def main():
         UB = user_set_board()
         CB = comp_set_board()
         while(not end):
+            hit = False
             print("1:Attack")
             print("2:View Damage Board")
             print("3:View Target Board")
@@ -179,9 +218,11 @@ def main():
                     end = strike[0]
                     hit=strike[2]
                     TB = strike[1]
+                    hit = False
                     defe = comp_attack()
                     end=defe[0]
                     UB=defe[1]
+                    hit = defe[2]
             elif(c == 2):
                 print("   0     1    2    3    4    5    6    7    8    9")      
                 rows =0     
